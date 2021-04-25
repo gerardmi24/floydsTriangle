eb9a3151a92dee5445d3ef07e03179d7f1de5623
gerardmi24 <gerardmistretta@yahoo.com>
2021-04-25 14:58:31 -0400

    Complete

--- floyds_triangle/src/App.js
+++ floyds_triangle/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Floyd from './Floyd.js';
 
 function App() {
+ 
+  function triangle() {
+    let count = 1
+    let rows = 8
+
+    for(let i = 1; i <= rows; i++) {
+        for(let j = 1; j <= i; j++, count++)
+            document.write(count + " ");
+            document.write("<br/ > \n");
+    }
+}
+
   return (
     <div className="App">
       <h1 className="App-header"> Floyd's Triangle </h1>
-
+      <Floyd tri={triangle()} />
     </div>
   );
 }
---
+++ floyds_triangle/src/Floyd.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Floyd({tri}) {
+
+  function callTri() {
+      return tri
+  }
+
+
+    return (
+        <div>
+            {callTri()}
+        </div>
+    )
+}
