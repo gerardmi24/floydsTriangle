44ac5dffc05ffefbf40d5c4fba23315e3012e9e8
gerardmi24 <gerardmistretta@yahoo.com>
2021-04-29 19:27:06 -0400

    Bugs fixed

--- floyds_triangle/src/Permutations.js
+++ floyds_triangle/src/Permutations.js
@@ -22,12 +22,12 @@ export default function Permutations({godWord}) {
                 // }
 
                 let restOfWord = godWord.slice(0, i) + godWord.slice(i + 1, godWord.length)
-                let newString = perms(restOfWord)
+                // let newString = perms(restOfWord)
                     // for (let permutes of Permutations(restOfWord)) {
                     //     arr.push(firstLetter + permutes)
                     // }
-                    for (let j = 0; j < newString.length; j++) {
-                        arr.push(firstLetter + newString + " ")
+                    for (let j = 0; j < restOfWord.length; j++) {
+                        arr.push(firstLetter + restOfWord + " ")
                     }
             }
             return arr;
