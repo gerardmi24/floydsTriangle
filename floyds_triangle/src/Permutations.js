d5f64d50bb7c4c115eb4fdf74620d50a33b53d39
gerardmi24 <gerardmistretta@yahoo.com>
2021-05-01 17:05:03 -0400

    No more dupes, but getting new error

--- floyds_triangle/src/Permutations.js
+++ floyds_triangle/src/Permutations.js
@@ -5,30 +5,29 @@ export default function Permutations({godWord}) {
     
     function perms() {
         let arr = []
-        console.log("arr", arr)
-
         if (godWord.length === 0) {
             return "";
         }
         if (godWord.length === 1 ) {
             return godWord;
         }
+    
         else {
             for (let i = 0; i < godWord.length; i++) {
                 let firstLetter = godWord[i]
+                // console.log("arr", arr)
 
-                // if (godWord.indexOf(firstLetter) != i) {
-                //     continue;
-                // }
-
+                if (godWord.indexOf(firstLetter) !== i)
+                    continue;
+                
                 let restOfWord = godWord.slice(0, i) + godWord.slice(i + 1, godWord.length)
-                // let newString = perms(restOfWord)
-                    // for (let permutes of Permutations(restOfWord)) {
-                    //     arr.push(firstLetter + permutes)
-                    // }
-                    for (let j = 0; j < restOfWord.length; j++) {
-                        arr.push(firstLetter + restOfWord + " ")
+
+                    for (let permutes of perms(restOfWord)) {
+                        arr.push(firstLetter + permutes + " ")
                     }
+                    // for (let j = 0; j < restOfWord.length; j++) {
+                    //     arr.push(firstLetter + restOfWord + " ")
+                    // }
             }
             return arr;
         }
